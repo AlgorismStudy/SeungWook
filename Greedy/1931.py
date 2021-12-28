724ba3537b1803d975cd25d77b30f361762190c2
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2021-12-28 20:32:26 +0900

    Update 1931.py

--- Greedy/1931.py
+++ Greedy/1931.py
@@ -3,7 +3,7 @@ import sys
 N = []
 for i in range(int(input())):
     N.append(list(map(int, sys.stdin.readline().split())))
-    N = sorted(N, key=lambda x: x[1])
+N = sorted(N, key=lambda x: x[1])
 
 final = 0
 conut = 0
