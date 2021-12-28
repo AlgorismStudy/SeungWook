1a0f969c45fa5a80f1f09550206119356cae615e
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2021-12-28 20:56:53 +0900

    Add files via upload

---
+++ Baekjoon/Greedy/1931.py
@@ -0,0 +1,16 @@
+import sys
+
+N = []
+for i in range(int(input())):
+    N.append(list(map(int, sys.stdin.readline().split())))
+
+N = sorted(N, key=lambda x: x[1])
+
+final = 0
+conut = 0
+for i, j in N:
+    if i >= final:
+        final = j
+        conut += 1
+
+print(conut)
