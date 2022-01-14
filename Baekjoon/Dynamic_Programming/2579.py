fd5e7e7a253af9faa404983a6479da0f32e02120
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2022-01-15 02:58:32 +0900

    Update 2579.py

--- Baekjoon/Dynamic_Programming/2579.py
+++ Baekjoon/Dynamic_Programming/2579.py
@@ -7,9 +7,10 @@ score = [int(input()) for _ in range(N)]
 
 a = [0] * 301
 a[1] = [score[0], score[0]]
-a[2] = [score[1], score[0]+score[1]]
-
-for i in range(3, N+1):
-    a[i] = [max(a[i-2])+score[i-1], a[i-1][0]+score[i-1]]
-    
-print(max(a[N]))
+if len(score) != 1:
+    a[2] = [score[1], score[0]+score[1]]
+    for i in range(3, N+1):
+        a[i] = [max(a[i-2])+score[i-1], a[i-1][0]+score[i-1]]
+    print(max(a[N]))
+else:
+    print(score[0])
