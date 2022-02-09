c7fd6cd93e55073bf5adc60168bc093ec9c2acac
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2022-02-09 21:38:25 +0900

    Update Main1026.java

--- Baekjoon_java/Greedy/Main1026.java
+++ Baekjoon_java/Greedy/Main1026.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main1026 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
