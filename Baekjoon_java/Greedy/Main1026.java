c4eabeab8437889cc3df338d3744aa1a6ef47f75
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2022-02-09 21:17:00 +0900

    Add files via upload

---
+++ Baekjoon_java/Greedy/Main1026.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int arr1[] = new int[N];
+        for (int i=0; i<N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr1);
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        int arr2[] = new int[N];
+        for (int i=0; i<N; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+        arr2 = Arrays.stream(arr2).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+
+        br.close();
+
+        int result = 0;
+        for (int i=0; i<N; i++) {
+            result += (arr1[i] * arr2[i]);
+        }
+
+        System.out.println(result);
+    }
+}
