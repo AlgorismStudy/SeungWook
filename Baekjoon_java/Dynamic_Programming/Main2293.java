8ebda83a16b1bcc1511219819370beb9d4797f58
longshort <tbvjqkdlcm1@naver.com>
2022-03-06 03:45:00 +0900

    git_test

---
+++ Baekjoon_java/Dynamic_Programming/Main2293.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2293 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[k+1];
+        arr[0] = 1;
+
+        for (int i=0; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            for (int j=num; j<=k; j++) {
+                arr[j] += arr[j - num];
+            }
+        }
+        br.close();
+        System.out.println(arr[k]);
+    }
+}
--- Baekjoon_java/Greedy/Main1026.java
+++ Baekjoon_java/Greedy/Main1026.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main1026 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
